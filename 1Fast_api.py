e78eccb56552900773cfd84c5773e231b08db48e
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2023-07-09 23:11:32 +0530

    Query parameters
    It is used to get details like searching from huge data. It does not requires exact endpoint.

--- 1Fast_api.py
+++ 1Fast_api.py
@@ -8,10 +8,21 @@ students = {
     3:{"name":"Rock","age":19,"class":"3rd year"}
 }
 
+# Get data
 @app.get("/")
 def home():
     return{"Result": "Home Page"}
 
+# Path parameter
 @app.get("/get-student/{std_id}")
 def get_student(std_id:int = Path(description="Enter the student id",gt=0,lt=4)):
     return students[std_id]
+
+# Query parameters
+@app.get("/get-by-name")
+def get_by_name(name:str):
+    for student_id in students:
+        print(students[student_id]['name'])
+        if students[student_id]['name'] == name:
+            return students[student_id]
+    return{f"{name}": "Not found"}
