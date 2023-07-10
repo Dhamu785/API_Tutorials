9ec95d1c0ffb27f9ddde130fabab87fb14b52bd1
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2023-07-10 11:49:32 +0530

    Post method for API
    Post method used to upload data to the database through API

--- 1Fast_api.py
+++ 1Fast_api.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI, Path
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Student(BaseModel):
+    name : str
+    age : int
+    year : str
+    
 students = {
-    1:{"name":"kali","age":18,"class":"2nd year"},
-    2:{"name":"John","age":17,"class":"1st year"},
-    3:{"name":"Rock","age":19,"class":"3rd year"}
+    1:{"name":"kali","age":18,"year":"2nd year"},
+    2:{"name":"John","age":17,"year":"1st year"},
+    3:{"name":"Rock","age":19,"year":"3rd year"}
 }
 
 # Get data
@@ -17,7 +23,7 @@ def home():
 # Path parameter
 @app.get("/get-student/{std_id}")
 # def get_student(std_id:int = None):
-def get_student(std_id:int = Path(description="Enter the student id",gt=0,lt=4)):
+def get_student(std_id:int = Path(description="Enter the student id",gt=0,lt=24)):
     return students[std_id]
 
 # Query parameters
@@ -38,3 +44,12 @@ def get_by_name(*, std_id:int , name: Optional[str]=None):
         if students[student_id]['name'] == name and student_id==std_id:
             return students[student_id]
     return{f"{name}": "Not found"}
+
+# Post method used to upload data to the database through API
+@app.post("/add-student/{student_id}")
+def add_student(student_id : int, student_details : Student):
+    if student_id in students:
+        return {"Error":"Id already exists"}
+    else:
+        students[student_id] = student_details
+        return{"Sucess":f"Student {student_details.name} added"}
